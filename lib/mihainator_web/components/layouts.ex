b98abc9b8a5312f0f4b25b7624ae831975af4396
Pascal Schmid <psschmid@gmx.de>
2024-10-06 23:40:48 +0200

    Initial commit

---
+++ lib/mihainator_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule MihainatorWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use MihainatorWeb, :controller` and
+  `use MihainatorWeb, :live_view`.
+  """
+  use MihainatorWeb, :html
+
+  embed_templates "layouts/*"
+end
---
+++ test/mihainator_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule MihainatorWeb.ErrorHTMLTest do
+  use MihainatorWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(MihainatorWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(MihainatorWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
