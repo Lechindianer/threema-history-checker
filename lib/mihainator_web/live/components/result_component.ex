b8f4c31bb7a4cfa52c109e7a18ec9ab935535e7d
Pascal Schmid <psschmid@gmx.de>
2024-10-20 23:31:02 +0200

    Dynamically build calendar

--- lib/mihainator_web/live/components/result_component.ex
+++ lib/mihainator_web/live/components/result_component.ex
@@ -7,11 +7,29 @@ defmodule MihainatorWeb.ResultComponent do
   def update(assigns, socket) do
     socket = assign(socket, assigns)
 
+    months = get_months(assigns.calendar_dates)
+
+    socket = assign(socket, months: months)
+
     {:ok, socket}
   end
 
   @impl true
   def mount(socket) do
     {:ok, socket}
+  end
+
+  def time_button(assigns) do
+    ~H"""
+    <button>
+      <time><%= @day %></time>
+    </button>
+    """
+  end
+
+  defp get_months(%{first_date: first_date}) do
+    start = Date.beginning_of_month(first_date)
+
+    for month_difference <- 0..11, do: Date.shift(start, month: month_difference)
   end
 end
